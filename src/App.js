6074fddae7aabfb445242ed08a51c4135295d2f8
John James Libron <johnjames.libron@gmail.com>
2022-03-01 16:11:32 +0800

    Add expenses and card components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses';
 
 function App() {
   const expenses = [
@@ -24,13 +24,7 @@ function App() {
   ];
 
   return (
-    <div>
-      <ExpenseItem 
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-    </div>
+    <Expenses items={expenses} />
   );
 }
 
